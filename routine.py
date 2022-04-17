fdc2a4dc4859dc90ba79a82db63a9eea87e10478
abrarabedin001 <60312096+abrarabedin001@users.noreply.github.com>
2022-04-17 13:49:30 +0600

    Add files via upload

---
+++ routine.py
@@ -0,0 +1,1103 @@
+
+cse221 = """CSE221	1	Mo(11:00 AM-12:20 PM-UB70301) Su(08:00 AM-09:20 AM-UB40101) Su(09:30 AM-10:50 AM-UB40101) We(11:00 AM-12:20 PM-UB70301)
+CSE221	2	Mo(12:30 PM-01:50 PM-UB70301) Su(11:00 AM-12:20 PM-UB40101) Su(12:30 PM-01:50 PM-UB40101) We(12:30 PM-01:50 PM-UB70301)
+CSE221	3	Mo(08:00 AM-09:20 AM-UB71101) Mo(09:30 AM-10:50 AM-UB71101) Su(12:30 PM-01:50 PM-UB70401) Tu(12:30 PM-01:50 PM-UB70401)
+CSE221	4	Mo(09:30 AM-10:50 AM-UB70302) Su(02:00 PM-03:20 PM-UB40101) Su(03:30 PM-04:50 PM-UB40101) We(09:30 AM-10:50 AM-UB70302)
+CSE221	5	Mo(03:30 PM-04:50 PM-UB70301) Su(08:00 AM-09:20 AM-UB40103) Su(09:30 AM-10:50 AM-UB40103) We(03:30 PM-04:50 PM-UB70301)
+CSE221	6	Mo(12:30 PM-01:50 PM-UB70302) Tu(08:00 AM-09:20 AM-UB71101) Tu(09:30 AM-10:50 AM-UB71101) We(12:30 PM-01:50 PM-UB70302)
+CSE221	7	Mo(02:00 PM-03:20 PM-UB70302) Tu(11:00 AM-12:20 PM-UB71101) Tu(12:30 PM-01:50 PM-UB71101) We(02:00 PM-03:20 PM-UB70302)
+CSE221	8	Mo(03:30 PM-04:50 PM-UB70302) Tu(02:00 PM-03:20 PM-UB71101) Tu(03:30 PM-04:50 PM-UB71101) We(03:30 PM-04:50 PM-UB70302)
+CSE221	9	Mo(08:00 AM-09:20 AM-UB70302) Tu(08:00 AM-09:20 AM-UB71102) Tu(09:30 AM-10:50 AM-UB71102) We(08:00 AM-09:20 AM-UB70302)
+CSE221	10	Mo(11:00 AM-12:20 PM-UB71101) Mo(12:30 PM-01:50 PM-UB71101) Su(08:00 AM-09:20 AM-UB70402) Tu(08:00 AM-09:20 AM-UB70402)
+CSE221	11	Mo(02:00 PM-03:20 PM-UB71101) Mo(03:30 PM-04:50 PM-UB71101) Su(09:30 AM-10:50 AM-UB70402) Tu(09:30 AM-10:50 AM-UB70402)
+CSE221	12	Mo(08:00 AM-09:20 AM-UB71102) Mo(09:30 AM-10:50 AM-UB71102) Su(11:00 AM-12:20 PM-UB50302) Tu(11:00 AM-12:20 PM-UB50302)
+CSE221	13	Su(12:30 PM-01:50 PM-UB30301) Tu(12:30 PM-01:50 PM-UB30301) We(08:00 AM-09:20 AM-UB71103) We(09:30 AM-10:50 AM-UB71103)
+CSE221	14	Su(02:00 PM-03:20 PM-UB70402) Tu(02:00 PM-03:20 PM-UB70402) We(11:00 AM-12:20 PM-UB71103) We(12:30 PM-01:50 PM-UB71103)
+CSE221	15	Su(03:30 PM-04:50 PM-UB30202) Tu(03:30 PM-04:50 PM-UB30202) We(02:00 PM-03:20 PM-UB71103) We(03:30 PM-04:50 PM-UB71103)
+CSE221	16	Su(12:30 PM-01:50 PM-UB70302) Tu(12:30 PM-01:50 PM-UB70302) We(08:00 AM-09:20 AM-UB40401) We(09:30 AM-10:50 AM-UB40401)"""
+
+cse251 = """CSE251	1	Sa(08:00 AM-09:20 AM-UB50203) Sa(09:30 AM-10:50 AM-UB50203) Su(02:00 PM-03:20 PM-UB70702) Tu(02:00 PM-03:20 PM-UB70702)
+CSE251	2	Sa(02:00 PM-03:20 PM-UB50203) Sa(03:30 PM-04:50 PM-UB50203) Su(03:30 PM-04:50 PM-UB70702) Tu(03:30 PM-04:50 PM-UB70702)
+CSE251	3	Mo(08:00 AM-09:20 AM-UB70701) Su(02:00 PM-03:20 PM-UB50202) Su(03:30 PM-04:50 PM-UB50202) We(08:00 AM-09:20 AM-UB70701)
+CSE251	4	Mo(09:30 AM-10:50 AM-UB70701) Su(08:00 AM-09:20 AM-UB50203) Su(09:30 AM-10:50 AM-UB50203) We(09:30 AM-10:50 AM-UB70701)
+CSE251	5	Mo(11:00 AM-12:20 PM-UB70701) Tu(08:00 AM-09:20 AM-UB50203) Tu(09:30 AM-10:50 AM-UB50203) We(11:00 AM-12:20 PM-UB70701)
+CSE251	6	Mo(12:30 PM-01:50 PM-UB70701) Tu(11:00 AM-12:20 PM-UB50203) Tu(12:30 PM-01:50 PM-UB50203) We(12:30 PM-01:50 PM-UB70701)
+CSE251	7	Mo(02:00 PM-03:20 PM-UB70701) Th(08:00 AM-09:20 AM-UB50203) Th(09:30 AM-10:50 AM-UB50203) We(02:00 PM-03:20 PM-UB70701)
+CSE251	8	Mo(03:30 PM-04:50 PM-UB70701) Th(11:00 AM-12:20 PM-UB50203) Th(12:30 PM-01:50 PM-UB50203) We(03:30 PM-04:50 PM-UB70701)
+CSE251	9	Mo(11:00 AM-12:20 PM-UB80103) Mo(12:30 PM-01:50 PM-UB80103) Su(08:00 AM-09:20 AM-UB71002) Tu(08:00 AM-09:20 AM-UB71002)
+CSE251	10	Mo(02:00 PM-03:20 PM-UB50203) Mo(03:30 PM-04:50 PM-UB50203) Su(09:30 AM-10:50 AM-UB71002) Tu(09:30 AM-10:50 AM-UB71002)
+CSE251	11	Su(11:00 AM-12:20 PM-UB71002) Tu(11:00 AM-12:20 PM-UB71002) We(08:00 AM-09:20 AM-UB50203) We(09:30 AM-10:50 AM-UB50203)
+CSE251	12	Su(12:30 PM-01:50 PM-UB70502) Tu(12:30 PM-01:50 PM-UB70502) We(11:00 AM-12:20 PM-UB50203) We(12:30 PM-01:50 PM-UB50203)
+CSE251	13	Su(02:00 PM-03:20 PM-UB70502) Th(02:00 PM-03:20 PM-UB50203) Th(03:30 PM-04:50 PM-UB50203) Tu(02:00 PM-03:20 PM-UB70502)
+CSE251	14	Sa(08:00 AM-09:20 AM-UB50201) Sa(09:30 AM-10:50 AM-UB50201) Su(03:30 PM-04:50 PM-UB70502) Tu(03:30 PM-04:50 PM-UB70502)"""
+
+
+mat216="""MAT216	1	Sa(09:30 AM-10:50 AM-UB70303) Th(09:30 AM-10:50 AM-UB70303) Tu(08:00 AM-09:20 AM-UB21511)
+MAT216	2	Sa(11:00 AM-12:20 PM-UB70303) Th(11:00 AM-12:20 PM-UB70303) Tu(09:30 AM-10:50 AM-UB21511)
+MAT216	3	Sa(12:30 PM-01:50 PM-UB70303) Th(12:30 PM-01:50 PM-UB70303) Tu(11:00 AM-12:20 PM-UB21511)
+MAT216	4	Sa(02:00 PM-03:20 PM-UB70303) Th(02:00 PM-03:20 PM-UB70303) Tu(12:30 PM-01:50 PM-UB21511)
+MAT216	5	Mo(02:00 PM-03:20 PM-UB21511) Su(09:30 AM-10:50 AM-UB70303) Tu(09:30 AM-10:50 AM-UB70303)
+MAT216	6	Mo(03:30 PM-04:50 PM-UB21511) Su(11:00 AM-12:20 PM-UB70303) Tu(11:00 AM-12:20 PM-UB70303)
+MAT216	7	Su(12:30 PM-01:50 PM-UB70303) Tu(12:30 PM-01:50 PM-UB70303) We(08:00 AM-09:20 AM-UB21511)
+MAT216	8	Su(03:30 PM-04:50 PM-UB70303) Tu(03:30 PM-04:50 PM-UB70303) We(09:30 AM-10:50 AM-UB21511)
+MAT216	9	Mo(09:30 AM-10:50 AM-UB70303) Th(11:00 AM-12:20 PM-UB21511) We(09:30 AM-10:50 AM-UB70303)
+MAT216	10	Mo(11:00 AM-12:20 PM-UB70303) Th(12:30 PM-01:50 PM-UB21511) We(11:00 AM-12:20 PM-UB70303)
+MAT216	11	Mo(12:30 PM-01:50 PM-UB70303) Th(02:00 PM-03:20 PM-UB21511) We(12:30 PM-01:50 PM-UB70303)
+MAT216	12	Mo(02:00 PM-03:20 PM-UB70303) Th(03:30 PM-04:50 PM-UB21511) We(02:00 PM-03:20 PM-UB70303)"""
+
+cse330="""CSE330	1	Sa(02:00 PM-03:20 PM-UB40101) Sa(03:30 PM-04:50 PM-UB40101) Su(11:00 AM-12:20 PM-UB70501) Tu(11:00 AM-12:20 PM-UB70501)
+CSE330	2	Sa(08:00 AM-09:20 AM-UB40103) Sa(09:30 AM-10:50 AM-UB40103) Su(12:30 PM-01:50 PM-UB70501) Tu(12:30 PM-01:50 PM-UB70501)
+CSE330	3	Sa(11:00 AM-12:20 PM-UB40103) Sa(12:30 PM-01:50 PM-UB40103) Su(02:00 PM-03:20 PM-UB71002) Tu(02:00 PM-03:20 PM-UB71002)
+CSE330	4	Sa(02:00 PM-03:20 PM-UB40103) Sa(03:30 PM-04:50 PM-UB40103) Su(03:30 PM-04:50 PM-UB71002) Tu(03:30 PM-04:50 PM-UB71002)
+CSE330	5	Mo(08:00 AM-09:20 AM-UB70501) Su(11:00 AM-12:20 PM-UB40103) Su(12:30 PM-01:50 PM-UB40103) We(08:00 AM-09:20 AM-UB70501)
+CSE330	6	Mo(11:00 AM-12:20 PM-UB70501) Su(08:00 AM-09:20 AM-UB40203) Su(09:30 AM-10:50 AM-UB40203) We(11:00 AM-12:20 PM-UB70501)
+CSE330	7	Sa(02:00 PM-03:20 PM-UB70701) Su(11:00 AM-12:20 PM-UB40203) Su(12:30 PM-01:50 PM-UB40203) Th(02:00 PM-03:20 PM-UB70701)
+CSE330	8	Sa(03:30 PM-04:50 PM-UB70701) Su(02:00 PM-03:20 PM-UB40203) Su(03:30 PM-04:50 PM-UB40203) Th(03:30 PM-04:50 PM-UB70701)
+CSE330	9	Sa(02:00 PM-03:20 PM-UB80102) Sa(03:30 PM-04:50 PM-UB80102) Sa(08:00 AM-09:20 AM-UB70701) Th(08:00 AM-09:20 AM-UB70701)
+CSE330	10	Sa(02:00 PM-03:20 PM-UB71002) Th(02:00 PM-03:20 PM-UB71002) We(11:00 AM-12:20 PM-UB40401) We(12:30 PM-01:50 PM-UB40401)
+CSE330	11	Mo(11:00 AM-12:20 PM-UB71103) Mo(12:30 PM-01:50 PM-UB71103) Su(03:30 PM-04:50 PM-UB80302) Tu(03:30 PM-04:50 PM-UB80302)
+CSE330	12	Mo(08:00 AM-09:20 AM-UB80301) Sa(08:00 AM-09:20 AM-UB80102) Sa(09:30 AM-10:50 AM-UB80102) We(08:00 AM-09:20 AM-UB80301)
+CSE330	13	Mo(09:30 AM-10:50 AM-UB80301) Sa(11:00 AM-12:20 PM-UB71103) Sa(12:30 PM-01:50 PM-UB71103) We(09:30 AM-10:50 AM-UB80301)
+CSE330	14	Mo(12:30 PM-01:50 PM-UB80301) Sa(11:00 AM-12:20 PM-UB80102) Sa(12:30 PM-01:50 PM-UB80102) We(12:30 PM-01:50 PM-UB80301)
+CSE330	15	Mo(02:00 PM-03:20 PM-UB80301) Tu(08:00 AM-09:20 AM-UB80102) Tu(09:30 AM-10:50 AM-UB80102) We(02:00 PM-03:20 PM-UB80301)
+CSE330	16	Sa(02:00 PM-03:20 PM-UB50301) Su(11:00 AM-12:20 PM-UB71103) Su(12:30 PM-01:50 PM-UB71103) Th(02:00 PM-03:20 PM-UB50301)
+CSE330	17	Sa(03:30 PM-04:50 PM-UB50301) Th(03:30 PM-04:50 PM-UB50301) Tu(11:00 AM-12:20 PM-UB60804) Tu(12:30 PM-01:50 PM-UB60804)
+CSE330	18	Sa(08:00 AM-09:20 AM-UB50302) Th(08:00 AM-09:20 AM-UB50302) We(11:00 AM-12:20 PM-UB80101) We(12:30 PM-01:50 PM-UB80101)
+CSE330	19	Sa(03:30 PM-04:50 PM-UB70702) Su(02:00 PM-03:20 PM-UB80403) Su(03:30 PM-04:50 PM-UB80403) Tu(03:30 PM-04:50 PM-UB70702)
+CSE330	21	Mo(11:00 AM-12:20 PM-UB60806) Mo(12:30 PM-01:50 PM-UB60806) Su(11:00 AM-12:20 PM-UB30403) Tu(11:00 AM-12:20 PM-UB30403)
+CSE330	22	Su(08:00 AM-09:20 AM-UB30702) Tu(08:00 AM-09:20 AM-UB30702) We(02:00 PM-03:20 PM-UB60806) We(03:30 PM-04:50 PM-UB60806)"""
+# print([1,2,4]==[4,2,1])
+
+import re
+arrCse221 = cse221.replace("\t"," ").split("CSE221")
+course = "CSE221"
+arr2Cse221 = []
+for r in range(1,len(arrCse221)):
+    if "08:00 AM-09:20 AM" not in arrCse221[r]:
+
+        word = arrCse221[r].replace("\n","").replace(")",") ,").replace("-"," ")
+        section = course+"_"+word[1:3]
+        word = word[3:]
+        word2 = re.sub("\sUB[0-9][0-9][0-9][0-9][0-9]","",word).split(",")
+        word2.pop()
+        word2.insert(0,section)
+
+
+        arr2Cse221.append(word2)
+
+# print(arr2Cse221)
+
+arrCse251 = cse251.replace("\t"," ").split("CSE251")
+course = "CSE251"
+arr2Cse251 = []
+for r in range(1,len(arrCse251)):
+    if "08:00 AM-09:20 AM" not in arrCse251[r]:
+
+        word = arrCse251[r].replace("\n","").replace(")",") ,").replace("-"," ")
+        section = course+"_"+word[1:3]
+        word = word[3:]
+        word2 = re.sub("\sUB[0-9][0-9][0-9][0-9][0-9]","",word).split(",")
+        word2.pop()
+        word2.insert(0,section)
+
+
+        arr2Cse251.append(word2)
+
+# print(arr2Cse251)
+
+arrMat216 = mat216.replace("\t"," ").split("MAT216")
+course = "Mat216"
+arr2Mat216 = []
+for r in range(1,len(arrMat216)):
+    if "08:00 AM-09:20 AM" not in arrMat216[r]:
+
+        word = arrMat216[r].replace("\n","").replace(")",") ,").replace("-"," ")
+        section = course+"_"+word[1:3]
+        word = word[3:]
+        word2 = re.sub("\sUB[0-9][0-9][0-9][0-9][0-9]","",word).split(",")
+        word2.pop()
+        word2.insert(0,section)
+
+
+        arr2Mat216.append(word2)
+
+# print(arr2Mat216)
+#
+arrCse330 = cse330.replace("\t"," ").split("CSE330")
+course = "CSe330"
+arr2Cse330 = []
+for r in range(1,len(arrCse330)):
+    if "08:00 AM-09:20 AM" not in arrCse330[r]:
+
+        word = arrCse330[r].replace("\n","").replace(")",") ,").replace("-"," ")
+        section = course+"_"+word[1:3]
+        word = word[3:]
+
+        word2 = re.sub("\sUB[0-9][0-9][0-9][0-9][0-9]","",word).split(",")
+        word2.pop()
+        # print(word2)
+        word2.insert(0,section)
+
+
+        arr2Cse330.append(word2)
+
+# print(arr2Cse330)
+a = {1,2,4}
+b = {6,7,8,1}
+c = {99,1}
+print(a&b&c)
+
+f = [{1,2,4,5}]
+print({2,4,1,5} in f)
+
+lastArr = []
+for a in arr2Cse251:
+
+    for b in arr2Cse330:
+        for c in arr2Cse221:
+            for d in arr2Mat216:
+
+                if len(set(a)&set(b))==0:
+                    comb1 = set(a).union(b)
+                    if len(comb1 & set(c))==0:
+                        comb2 = comb1.union(c)
+
+                        if len(comb2 & set(d))==0:
+                            # print(a[0],b[0])
+                            str4 = str(a[0])+str(b[0])+str(c[0])+str(d[0])
+                            lastArr.append(str4)
+
+
+                # if(set(a) & set(b) & set(c) & set(d)):
+                #     pass
+                # else:
+                #     print(a,b,c,d)
+print(len(lastArr))
+print(len(arr2Cse221)*len(arr2Cse251)*len(arr2Cse330)*len(arr2Mat216))
+print(lastArr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
